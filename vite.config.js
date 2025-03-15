284004b7a0695d8a05f1350a56fb9a2a67dedc4f
Robert Gleason <l2ob122@gmail.com>
2025-03-14 20:00:50 -0700

    gh

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2025-portfolio/', // Your actual repository name
+  base: '/2025-portfolio', // Your actual repository name
 });
