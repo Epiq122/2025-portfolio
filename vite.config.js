1a59ca2f6d32ec0b08d860193dbb8852afd806aa
Robert Gleason <l2ob122@gmail.com>
2025-03-15 12:31:52 -0700

    changed to vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/2025-portfolio', // Your actual repository name
 });
